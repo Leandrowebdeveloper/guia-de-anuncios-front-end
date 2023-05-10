4432088bb41e9accab40babc8e39ed5d6b6fc58a
Leandro Alves de Souza <leandro.web.developer.10@gmail.com>
2023-05-10 10:27:22 -0300

    Alterado o footer parao componente ion-segment

--- src/app/footer/footer.page.ts
+++ src/app/footer/footer.page.ts
@@ -1,27 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { IonSegment, NavController } from '@ionic/angular';
+import { Observable, Subscription } from 'rxjs';
 import { User } from '../interface';
 import { AuthService } from '../services/auth/auth.service';
 import { Keyboard } from '@capacitor/keyboard';
 import { Capacitor } from '@capacitor/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.page.html',
   styleUrls: ['./footer.page.scss'],
 })
-export class FooterPage implements OnInit {
+export class FooterPage implements OnInit, OnDestroy {
+  @ViewChild(IonSegment, { static: false }) segment!: IonSegment;
   public user$!: Observable<User | void>;
   public isFooter!: boolean;
+  private $router!: Subscription;
   constructor(
     private authService: AuthService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private router: Router
   ) {}
+
+  ngOnDestroy(): void {
+    this.$router.unsubscribe();
+  }
 
   ngOnInit() {
     this.toggleFooter();
     this.getUser();
+    this.initSegment();
+  }
+
+  private initSegment() {
+    this.$router = this.router.events.subscribe((router: any): void => {
+      this.setSegment(router);
+    });
+  }
+
+  private setSegment(router: any) {
+    if (router instanceof NavigationEnd) {
+      switch (router.url) {
+        case '/entrar':
+          this.segment.value = 'entrar';
+          break;
+        case '/cadastrar':
+          this.segment.value = 'cadastrar';
+          break;
+        case '/inicio':
+          this.segment.value = 'inicio';
+          break;
+        case '/categoria/classificados':
+          this.segment.value = 'classificados';
+          break;
+        case '/painel-de-controle/admin/usuario':
+          this.segment.value = 'usuario';
+          break;
+        case '/painel-de-controle/anuncio':
+          this.segment.value = 'anuncio';
+          break;
+      }
+    }
   }
 
   private toggleFooter() {
